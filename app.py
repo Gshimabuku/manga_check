b57665a3b8471d706d14d2f326651e007e193702
Gshimabuku <123082798+Gshimabuku@users.noreply.github.com>
2025-11-27 10:44:23 +0900

    date

--- app.py
+++ app.py
@@ -62,7 +62,6 @@ def get_gspread_client():
 # --- 日付判定 ---
 def is_past(date_str: str) -> bool:
     today = datetime.now().date()
-    st.info(today)
 
     # --- 1) 日付ありパターン（YYYY年MM月DD日） ---
     full_date = re.match(r"(\d{4}年\d{1,2}月\d{1,2}日)", date_str)
